de8f72f908f51a4129669b7c6cc24f828a3dcfab
Felix Xing <felixaszx@outlook.com>
2025-12-12 20:32:19 -0500

    add nonexcepts

--- src/stb_perlin.cxx
+++ src/stb_perlin.cxx
@@ -1,47 +1,303 @@
-#include "stb_perlin.hxx"
-#define STB_PERLIN_IMPLEMENTATION
-#include <stb/stb_perlin.h>
+#include "stb_image.hxx"
 
-float //
-stb::perlin_noise_generator::noise(std::array<int, 3> wrap) noexcept
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+extern "C"
 {
-    return stb_perlin_noise3(coord_[0], coord_[1], coord_[2], //
-                             wrap[0], wrap[1], wrap[2]);
+    static int //
+    io_callbacks_bridge_read(void* cb, char* data, int size) noexcept
+    {
+        auto func = reinterpret_cast<stb::image::io_callbacks*>(cb)->read_;
+        return func(reinterpret_cast<std::byte*>(data), size);
+    }
+
+    static void //
+    io_callbacks_bridge_skip(void* cb, int n) noexcept
+    {
+        auto func = reinterpret_cast<stb::image::io_callbacks*>(cb)->skip_;
+        func(n);
+    }
+
+    static int //
+    io_callbacks_bridge_eof(void* cb) noexcept
+    {
+        auto func = reinterpret_cast<stb::image::io_callbacks*>(cb)->eof_;
+        return func();
+    }
 }
 
-float                                        //
-stb::perlin_noise_generator::noise(int seed, //
-                                   std::array<float, 3> wrap) noexcept
+void //
+stb::image::set(loading_states name, bool state)
 {
-    return stb_perlin_noise3_seed(coord_[0], coord_[1], coord_[2], //
-                                  wrap[0], wrap[1], wrap[2],       //
-                                  seed);
+    switch (name)
+    {
+        case loading_states::unpremultiply:
+        {
+            stbi_set_unpremultiply_on_load(state);
+            break;
+        }
+        case loading_states::iphone_png_to_rgb:
+        {
+            stbi_convert_iphone_png_to_rgb(state);
+            break;
+        }
+        case loading_states::flip_vertically:
+        {
+            stbi_set_flip_vertically_on_load(state);
+            break;
+        }
+    }
 }
 
-float                                                      //
-stb::perlin_noise_generator::ridge_noise(float lacunarity, //
-                                         float gain,
-                                         float offset,
-                                         int octaves) noexcept
+void //
+stb::image::set(hdr_to_ldr name, float value)
 {
-    return stb_perlin_ridge_noise3(coord_[0], coord_[1], coord_[2], //
-                                   lacunarity, gain, offset, octaves);
+    switch (name)
+    {
+        case hdr_to_ldr::gama:
+        {
+            stbi_hdr_to_ldr_gamma(value);
+            break;
+        }
+        case hdr_to_ldr::scale:
+        {
+            stbi_hdr_to_ldr_scale(value);
+            break;
+        }
+    }
 }
 
-float                                                    //
-stb::perlin_noise_generator::fbm_noise(float lacunarity, //
-                                       float gain,
-                                       int octaves) noexcept
+void //
+stb::image::set(ldr_to_hdr name, float value)
 {
-    return stb_perlin_fbm_noise3(coord_[0], coord_[1], coord_[2], //
-                                 lacunarity, gain, octaves);
+    switch (name)
+    {
+        case ldr_to_hdr::gama:
+        {
+            stbi_ldr_to_hdr_gamma(value);
+            break;
+        }
+        case ldr_to_hdr::scale:
+        {
+            stbi_ldr_to_hdr_scale(value);
+            break;
+        }
+    }
 }
 
-float                                                           //
-stb::perlin_noise_generator::turbulence_noise(float lacunarity, //
-                                              float gain,
-                                              int octaves) noexcept
+void //
+stb::image::this_thread::set(loading_states name, bool state)
 {
-    return stb_perlin_turbulence_noise3(coord_[0], coord_[1], coord_[2], //
-                                        lacunarity, gain, octaves);
+    switch (name)
+    {
+        case loading_states::unpremultiply:
+        {
+            stbi_set_unpremultiply_on_load_thread(state);
+            break;
+        }
+        case loading_states::iphone_png_to_rgb:
+        {
+            stbi_convert_iphone_png_to_rgb_thread(state);
+            break;
+        }
+        case loading_states::flip_vertically:
+        {
+            stbi_set_flip_vertically_on_load_thread(state);
+            break;
+        }
+    }
+}
+
+std::byte*                                //
+stb::image::load(const std::byte* buffer, //
+                 int len,
+                 info& info,
+                 int desired_channels) noexcept
+{
+    return reinterpret_cast<std::byte*>(                                //
+        stbi_load_from_memory(reinterpret_cast<const stbi_uc*>(buffer), //
+                              len, &info.x_, &info.y_,                  //
+                              &info.comp_, desired_channels));
+}
+
+std::byte*                               //
+stb::image::load(io_callbacks callbacks, //
+                 info& info,
+                 int desired_channels) noexcept
+{
+    stbi_io_callbacks stb_cb = {};
+    stb_cb.read = io_callbacks_bridge_read;
+    stb_cb.skip = io_callbacks_bridge_skip;
+    stb_cb.eof = io_callbacks_bridge_eof;
+    return reinterpret_cast<std::byte*>(              //
+        stbi_load_from_callbacks(&stb_cb, &callbacks, //
+                                 &info.x_, &info.y_,  //
+                                 &info.comp_, desired_channels));
+}
+
+std::byte*                                              //
+stb::image::load(const std::filesystem::path& filename, //
+                 info& info,
+                 int desired_channels) noexcept
+{
+    std::string name_str = filename.string();
+    return reinterpret_cast<std::byte*>( //
+        stbi_load(name_str.c_str(),      //
+                  &info.x_, &info.y_,    //
+                  &info.comp_, desired_channels));
+}
+
+std::byte*                     //
+stb::image::load(std::FILE* f, //
+                 info& info,
+                 int desired_channels) noexcept
+{
+    return reinterpret_cast<std::byte*>(        //
+        stbi_load_from_file(f,                  //
+                            &info.x_, &info.y_, //
+                            &info.comp_, desired_channels));
+}
+
+std::byte*                                    //
+stb::image::load_gif(const std::byte* buffer, //
+                     int len,
+                     std::vector<int*>& delays,
+                     info& info,
+                     int& z,
+                     int req_comp) noexcept
+{
+    return reinterpret_cast<std::byte*>(                                      //
+        stbi_load_gif_from_memory(reinterpret_cast<const stbi_uc*>(buffer),   //
+                                  len, delays.data(), &info.x_, &info.y_, &z, //
+                                  &info.comp_, req_comp));
+}
+
+float*                                     //
+stb::image::loadf(const std::byte* buffer, //
+                  int len,
+                  info& info,
+                  int desired_channels) noexcept
+{
+    return stbi_loadf_from_memory(reinterpret_cast<const stbi_uc*>(buffer), //
+                                  len, &info.x_, &info.y_,                  //
+                                  &info.comp_, desired_channels);
+}
+
+float*                                    //
+stb::image::loadf(io_callbacks callbacks, //
+                  info& info,
+                  int desired_channels) noexcept
+{
+    stbi_io_callbacks stb_cb = {};
+    stb_cb.read = io_callbacks_bridge_read;
+    stb_cb.skip = io_callbacks_bridge_skip;
+    stb_cb.eof = io_callbacks_bridge_eof;
+    return stbi_loadf_from_callbacks(&stb_cb, &callbacks, //
+                                     &info.x_, &info.y_,  //
+                                     &info.comp_, desired_channels);
+}
+
+float*                                                   //
+stb::image::loadf(const std::filesystem::path& filename, //
+                  info& info,
+                  int desired_channels) noexcept
+{
+    std::string name_str = filename.string();
+    return stbi_loadf(name_str.c_str(),   //
+                      &info.x_, &info.y_, //
+                      &info.comp_, desired_channels);
+}
+
+float*                          //
+stb::image::loadf(std::FILE* f, //
+                  info& info,
+                  int desired_channels) noexcept
+{
+    return stbi_loadf_from_file(f,                  //
+                                &info.x_, &info.y_, //
+                                &info.comp_, desired_channels);
+}
+
+bool //
+stb::image::is_hdr(io_callbacks callbacks) noexcept
+{
+    stbi_io_callbacks stb_cb = {};
+    stb_cb.read = io_callbacks_bridge_read;
+    stb_cb.skip = io_callbacks_bridge_skip;
+    stb_cb.eof = io_callbacks_bridge_eof;
+    return stbi_is_hdr_from_callbacks(&stb_cb, &callbacks);
+}
+
+bool                                        //
+stb::image::is_hdr(const std::byte* buffer, //
+                   int len) noexcept
+{
+    return stbi_is_hdr_from_memory(reinterpret_cast<const stbi_uc*>(buffer), //
+                                   len);
+}
+
+bool //
+stb::image::is_hdr(const std::filesystem::path& filename) noexcept
+{
+    std::string name_str = filename.string();
+    return stbi_is_hdr(name_str.c_str());
+}
+
+bool //
+stb::image::is_hdr(std::FILE* f) noexcept
+{
+    return stbi_is_hdr_from_file(f);
+}
+
+const char* //
+stb::image::failure_reason() noexcept
+{
+    const char* err = stbi_failure_reason();
+    return err == nullptr ? "stb::image: no error" : err;
+}
+
+void //
+stb::image::free(void* load_result) noexcept
+{
+    stbi_image_free(load_result);
+}
+
+stb::image::info                               //
+stb::image::read_info(const std::byte* buffer, //
+                      int len) noexcept
+{
+    info i = {};
+    stbi_info_from_memory(reinterpret_cast<const stbi_uc*>(buffer), //
+                          len, &i.x_, &i.y_, &i.comp_);
+    return i;
+}
+
+stb::image::info //
+stb::image::read_info(io_callbacks callbacks) noexcept
+{
+    info i = {};
+    stbi_io_callbacks stb_cb = {};
+    stb_cb.read = io_callbacks_bridge_read;
+    stb_cb.skip = io_callbacks_bridge_skip;
+    stb_cb.eof = io_callbacks_bridge_eof;
+    stbi_info_from_callbacks(&stb_cb, &callbacks, &i.x_, &i.y_, &i.comp_);
+    return i;
+}
+
+stb::image::info //
+stb::image::read_info(const std::filesystem::path& filename) noexcept
+{
+    info i = {};
+    std::string name_str = filename.string();
+    stbi_info(name_str.c_str(), &i.x_, &i.y_, &i.comp_);
+    return i;
+}
+
+stb::image::info //
+stb::image::read_info(std::FILE* f) noexcept
+{
+    info i = {};
+    stbi_info_from_file(f, &i.x_, &i.y_, &i.comp_);
+    return i;
 }
